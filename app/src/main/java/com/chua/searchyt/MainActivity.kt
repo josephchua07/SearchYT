620c127ebb694d7b1d872fc3bcf02d212677ae68
Joseph Stephen Chua <josephstephensumampongchua@gmail.com>
2023-09-18 12:08:02 +0800

    setup view binding

--- app/src/main/java/com/chua/searchyt/MainActivity.kt
+++ app/src/main/java/com/chua/searchyt/MainActivity.kt
@@ -2,12 +2,17 @@ package com.chua.searchyt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chua.searchyt.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/chua/searchyt/ui/search/SearchFragment.kt
+++ app/src/main/java/com/chua/searchyt/ui/search/SearchFragment.kt
@@ -6,24 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.chua.searchyt.R
+import com.chua.searchyt.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
     private val searchViewModel: SearchViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         searchViewModel.search("android developer")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
